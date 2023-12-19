de83ca247633512a238d3982f98dd8e5c25d2fc6
DevvratSahu07 <devvratsahu223@gmail.com>
2023-12-19 14:19:23 +0530

    homepage created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+// import NavbarContainer from './components/layout/Navbar/NavbarContainer'
+import LandingPage from './components/layout/LandingPage'
+function App() {
+  return (
+    <div>
+      {/* <NavbarContainer/> */}
+      <LandingPage/>
+    </div>
+
+  )
+}
+
+
+
+export default App
---
+++ src/components/layout/LandingPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavbarContainer from './Navbar/NavbarContainer'
+import ImageSlider from './imageSlider'
+import HomePageProductContainer from '../HomePageProduct/HomePageProductContainer'
+import Footer from './Footer'
+function LandingPage() {
+  return (
+   <React.Fragment>
+        <NavbarContainer/>
+        <div className="container-fluid px-0">
+            <ImageSlider/>
+            <HomePageProductContainer/>
+            <Footer/>
+        </div>
+   </React.Fragment>
+  )
+}
+
+export default LandingPage; 
